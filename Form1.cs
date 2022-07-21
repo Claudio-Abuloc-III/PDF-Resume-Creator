39fc4e05648d851167d2af41ab54e4a8fc3f766c
Claudio-Abuloc-III <abulociiiclaudio@gmail.com>
2022-07-22 03:39:29 +0800

    Try masave yung info sa Json File

--- Form1.cs
+++ Form1.cs
@@ -21,19 +21,21 @@ namespace PDF_Resume_Creator
 
         private void Savebutton_Click(object sender, EventArgs e)
         {
-            StreamWriter file = new StreamWriter(@"D:\Claudio Abuloc Files\Contact Tracing\Resume.txt", true);
+            
+            StreamWriter file = new StreamWriter(@"D:\Claudio Abuloc Files\Contact Tracing\Resume.json", true);
             file.WriteLine("Name: " + FirstNametextBox.Text + SurnametextBox.Text);
             file.WriteLine("Phone: " + PhonetextBox.Text);
             file.WriteLine("Email: " + EmailtextBox.Text);
             file.WriteLine("Home: " + HometextBox.Text);
             file.WriteLine("Skills: " + SkillstextBox.Text);
-            file.WriteLine("Elementary School: " + ElementarytextBox.Text + SYElementarytextBox.Text);
-            file.WriteLine("High School: " + HighSchooltextBox.Text + SYHighschooltextBox.Text);
-            file.WriteLine("College/University: " + CollegetextBox.Text + SYCollegetextBox.Text);
+            file.WriteLine("Elementary School: " + ElementarytextBox.Text + "" + SYElementarytextBox.Text);
+            file.WriteLine("High School: " + HighSchooltextBox.Text + "" + SYHighschooltextBox.Text);
+            file.WriteLine("College/University: " + CollegetextBox.Text + "" + SYCollegetextBox.Text);
             file.WriteLine("Awards: " + AwardstextBox.Text);
             file.WriteLine("Certification: " + CertificationtextBox.Text);
             file.WriteLine("Work Experience: " + WorkExperiencetextBox.Text);
             file.Close();
+            FirstNametextBox.Text = "";
         }
     }
 }
