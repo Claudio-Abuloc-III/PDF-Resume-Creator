cc669e48f7edf58e545e06dd34a4258339228e48
Claudio-Abuloc-III <abulociiiclaudio@gmail.com>
2022-07-22 03:13:53 +0800

    All resume info needed, example education, contact, etc (Labels and Textboxes. I added a Save button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PDF_Resume_Creator
 {
-    partial class Form1
+    partial class PDFResumeForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,396 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FirstNamelabel = new System.Windows.Forms.Label();
+            this.FirstNametextBox = new System.Windows.Forms.TextBox();
+            this.Surnamelabel = new System.Windows.Forms.Label();
+            this.SurnametextBox = new System.Windows.Forms.TextBox();
+            this.Phonelabel = new System.Windows.Forms.Label();
+            this.PhonetextBox = new System.Windows.Forms.TextBox();
+            this.Emaillabel = new System.Windows.Forms.Label();
+            this.EmailtextBox = new System.Windows.Forms.TextBox();
+            this.Homelabel = new System.Windows.Forms.Label();
+            this.HometextBox = new System.Windows.Forms.TextBox();
+            this.Contactlabel = new System.Windows.Forms.Label();
+            this.Skillslabel = new System.Windows.Forms.Label();
+            this.SkillstextBox = new System.Windows.Forms.TextBox();
+            this.Educationlabel = new System.Windows.Forms.Label();
+            this.ElementarytextBox = new System.Windows.Forms.TextBox();
+            this.Elementarylabel = new System.Windows.Forms.Label();
+            this.SYlabel = new System.Windows.Forms.Label();
+            this.SYElementarytextBox = new System.Windows.Forms.TextBox();
+            this.HighSchoollabel = new System.Windows.Forms.Label();
+            this.HighSchooltextBox = new System.Windows.Forms.TextBox();
+            this.SYHighschooltextBox = new System.Windows.Forms.TextBox();
+            this.Collegelabel = new System.Windows.Forms.Label();
+            this.CollegetextBox = new System.Windows.Forms.TextBox();
+            this.SYCollegetextBox = new System.Windows.Forms.TextBox();
+            this.WorkExperiencelabel = new System.Windows.Forms.Label();
+            this.WorkExperiencetextBox = new System.Windows.Forms.TextBox();
+            this.Awardlabel = new System.Windows.Forms.Label();
+            this.AwardstextBox = new System.Windows.Forms.TextBox();
+            this.Certificationlabel = new System.Windows.Forms.Label();
+            this.CertificationtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Savebutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FirstNamelabel
+            // 
+            this.FirstNamelabel.AutoSize = true;
+            this.FirstNamelabel.Location = new System.Drawing.Point(29, 36);
+            this.FirstNamelabel.Name = "FirstNamelabel";
+            this.FirstNamelabel.Size = new System.Drawing.Size(72, 16);
+            this.FirstNamelabel.TabIndex = 0;
+            this.FirstNamelabel.Text = "First Name";
+            // 
+            // FirstNametextBox
+            // 
+            this.FirstNametextBox.Location = new System.Drawing.Point(107, 33);
+            this.FirstNametextBox.Name = "FirstNametextBox";
+            this.FirstNametextBox.Size = new System.Drawing.Size(389, 22);
+            this.FirstNametextBox.TabIndex = 1;
+            // 
+            // Surnamelabel
+            // 
+            this.Surnamelabel.AutoSize = true;
+            this.Surnamelabel.Location = new System.Drawing.Point(569, 36);
+            this.Surnamelabel.Name = "Surnamelabel";
+            this.Surnamelabel.Size = new System.Drawing.Size(61, 16);
+            this.Surnamelabel.TabIndex = 2;
+            this.Surnamelabel.Text = "Surname";
+            // 
+            // SurnametextBox
+            // 
+            this.SurnametextBox.Location = new System.Drawing.Point(636, 36);
+            this.SurnametextBox.Name = "SurnametextBox";
+            this.SurnametextBox.Size = new System.Drawing.Size(368, 22);
+            this.SurnametextBox.TabIndex = 3;
+            // 
+            // Phonelabel
+            // 
+            this.Phonelabel.AutoSize = true;
+            this.Phonelabel.Location = new System.Drawing.Point(29, 109);
+            this.Phonelabel.Name = "Phonelabel";
+            this.Phonelabel.Size = new System.Drawing.Size(46, 16);
+            this.Phonelabel.TabIndex = 4;
+            this.Phonelabel.Text = "Phone";
+            // 
+            // PhonetextBox
+            // 
+            this.PhonetextBox.Location = new System.Drawing.Point(107, 109);
+            this.PhonetextBox.Name = "PhonetextBox";
+            this.PhonetextBox.Size = new System.Drawing.Size(191, 22);
+            this.PhonetextBox.TabIndex = 5;
+            // 
+            // Emaillabel
+            // 
+            this.Emaillabel.AutoSize = true;
+            this.Emaillabel.Location = new System.Drawing.Point(29, 141);
+            this.Emaillabel.Name = "Emaillabel";
+            this.Emaillabel.Size = new System.Drawing.Size(41, 16);
+            this.Emaillabel.TabIndex = 6;
+            this.Emaillabel.Text = "Email";
+            // 
+            // EmailtextBox
+            // 
+            this.EmailtextBox.Location = new System.Drawing.Point(107, 141);
+            this.EmailtextBox.Name = "EmailtextBox";
+            this.EmailtextBox.Size = new System.Drawing.Size(191, 22);
+            this.EmailtextBox.TabIndex = 7;
+            // 
+            // Homelabel
+            // 
+            this.Homelabel.AutoSize = true;
+            this.Homelabel.Location = new System.Drawing.Point(29, 177);
+            this.Homelabel.Name = "Homelabel";
+            this.Homelabel.Size = new System.Drawing.Size(44, 16);
+            this.Homelabel.TabIndex = 8;
+            this.Homelabel.Text = "Home";
+            // 
+            // HometextBox
+            // 
+            this.HometextBox.Location = new System.Drawing.Point(107, 177);
+            this.HometextBox.Name = "HometextBox";
+            this.HometextBox.Size = new System.Drawing.Size(191, 22);
+            this.HometextBox.TabIndex = 9;
+            // 
+            // Contactlabel
+            // 
+            this.Contactlabel.AutoSize = true;
+            this.Contactlabel.Location = new System.Drawing.Point(164, 81);
+            this.Contactlabel.Name = "Contactlabel";
+            this.Contactlabel.Size = new System.Drawing.Size(72, 16);
+            this.Contactlabel.TabIndex = 10;
+            this.Contactlabel.Text = "CONTACT";
+            // 
+            // Skillslabel
+            // 
+            this.Skillslabel.AutoSize = true;
+            this.Skillslabel.Location = new System.Drawing.Point(173, 231);
+            this.Skillslabel.Name = "Skillslabel";
+            this.Skillslabel.Size = new System.Drawing.Size(50, 16);
+            this.Skillslabel.TabIndex = 11;
+            this.Skillslabel.Text = "SKILLS";
+            // 
+            // SkillstextBox
+            // 
+            this.SkillstextBox.Location = new System.Drawing.Point(107, 250);
+            this.SkillstextBox.Multiline = true;
+            this.SkillstextBox.Name = "SkillstextBox";
+            this.SkillstextBox.Size = new System.Drawing.Size(191, 139);
+            this.SkillstextBox.TabIndex = 12;
+            // 
+            // Educationlabel
+            // 
+            this.Educationlabel.AutoSize = true;
+            this.Educationlabel.Location = new System.Drawing.Point(655, 81);
+            this.Educationlabel.Name = "Educationlabel";
+            this.Educationlabel.Size = new System.Drawing.Size(86, 16);
+            this.Educationlabel.TabIndex = 13;
+            this.Educationlabel.Text = "EDUCATION";
+            // 
+            // ElementarytextBox
+            // 
+            this.ElementarytextBox.Location = new System.Drawing.Point(462, 128);
+            this.ElementarytextBox.Multiline = true;
+            this.ElementarytextBox.Name = "ElementarytextBox";
+            this.ElementarytextBox.Size = new System.Drawing.Size(269, 22);
+            this.ElementarytextBox.TabIndex = 14;
+            // 
+            // Elementarylabel
+            // 
+            this.Elementarylabel.AutoSize = true;
+            this.Elementarylabel.Location = new System.Drawing.Point(376, 109);
+            this.Elementarylabel.Name = "Elementarylabel";
+            this.Elementarylabel.Size = new System.Drawing.Size(120, 16);
+            this.Elementarylabel.TabIndex = 15;
+            this.Elementarylabel.Text = "Elementary School";
+            // 
+            // SYlabel
+            // 
+            this.SYlabel.AutoSize = true;
+            this.SYlabel.Location = new System.Drawing.Point(797, 109);
+            this.SYlabel.Name = "SYlabel";
+            this.SYlabel.Size = new System.Drawing.Size(81, 16);
+            this.SYlabel.TabIndex = 16;
+            this.SYlabel.Text = "School Year";
+            // 
+            // SYElementarytextBox
+            // 
+            this.SYElementarytextBox.Location = new System.Drawing.Point(761, 128);
+            this.SYElementarytextBox.Name = "SYElementarytextBox";
+            this.SYElementarytextBox.Size = new System.Drawing.Size(218, 22);
+            this.SYElementarytextBox.TabIndex = 17;
+            // 
+            // HighSchoollabel
+            // 
+            this.HighSchoollabel.AutoSize = true;
+            this.HighSchoollabel.Location = new System.Drawing.Point(376, 159);
+            this.HighSchoollabel.Name = "HighSchoollabel";
+            this.HighSchoollabel.Size = new System.Drawing.Size(80, 16);
+            this.HighSchoollabel.TabIndex = 18;
+            this.HighSchoollabel.Text = "High School";
+            // 
+            // HighSchooltextBox
+            // 
+            this.HighSchooltextBox.Location = new System.Drawing.Point(462, 174);
+            this.HighSchooltextBox.Multiline = true;
+            this.HighSchooltextBox.Name = "HighSchooltextBox";
+            this.HighSchooltextBox.Size = new System.Drawing.Size(269, 22);
+            this.HighSchooltextBox.TabIndex = 19;
+            // 
+            // SYHighschooltextBox
+            // 
+            this.SYHighschooltextBox.Location = new System.Drawing.Point(761, 171);
+            this.SYHighschooltextBox.Name = "SYHighschooltextBox";
+            this.SYHighschooltextBox.Size = new System.Drawing.Size(218, 22);
+            this.SYHighschooltextBox.TabIndex = 20;
+            // 
+            // Collegelabel
+            // 
+            this.Collegelabel.AutoSize = true;
+            this.Collegelabel.Location = new System.Drawing.Point(376, 213);
+            this.Collegelabel.Name = "Collegelabel";
+            this.Collegelabel.Size = new System.Drawing.Size(123, 16);
+            this.Collegelabel.TabIndex = 21;
+            this.Collegelabel.Text = "College / University";
+            // 
+            // CollegetextBox
+            // 
+            this.CollegetextBox.Location = new System.Drawing.Point(462, 232);
+            this.CollegetextBox.Multiline = true;
+            this.CollegetextBox.Name = "CollegetextBox";
+            this.CollegetextBox.Size = new System.Drawing.Size(269, 22);
+            this.CollegetextBox.TabIndex = 22;
+            // 
+            // SYCollegetextBox
+            // 
+            this.SYCollegetextBox.Location = new System.Drawing.Point(761, 232);
+            this.SYCollegetextBox.Name = "SYCollegetextBox";
+            this.SYCollegetextBox.Size = new System.Drawing.Size(218, 22);
+            this.SYCollegetextBox.TabIndex = 23;
+            // 
+            // WorkExperiencelabel
+            // 
+            this.WorkExperiencelabel.AutoSize = true;
+            this.WorkExperiencelabel.Location = new System.Drawing.Point(376, 413);
+            this.WorkExperiencelabel.Name = "WorkExperiencelabel";
+            this.WorkExperiencelabel.Size = new System.Drawing.Size(136, 16);
+            this.WorkExperiencelabel.TabIndex = 24;
+            this.WorkExperiencelabel.Text = "WORK EXPERIENCE";
+            // 
+            // WorkExperiencetextBox
+            // 
+            this.WorkExperiencetextBox.Location = new System.Drawing.Point(462, 432);
+            this.WorkExperiencetextBox.Multiline = true;
+            this.WorkExperiencetextBox.Name = "WorkExperiencetextBox";
+            this.WorkExperiencetextBox.Size = new System.Drawing.Size(517, 164);
+            this.WorkExperiencetextBox.TabIndex = 25;
+            // 
+            // Awardlabel
+            // 
+            this.Awardlabel.AutoSize = true;
+            this.Awardlabel.Location = new System.Drawing.Point(376, 277);
+            this.Awardlabel.Name = "Awardlabel";
+            this.Awardlabel.Size = new System.Drawing.Size(67, 16);
+            this.Awardlabel.TabIndex = 26;
+            this.Awardlabel.Text = "AWARDS";
+            // 
+            // AwardstextBox
+            // 
+            this.AwardstextBox.Location = new System.Drawing.Point(462, 298);
+            this.AwardstextBox.Multiline = true;
+            this.AwardstextBox.Name = "AwardstextBox";
+            this.AwardstextBox.Size = new System.Drawing.Size(269, 91);
+            this.AwardstextBox.TabIndex = 27;
+            // 
+            // Certificationlabel
+            // 
+            this.Certificationlabel.AutoSize = true;
+            this.Certificationlabel.Location = new System.Drawing.Point(758, 277);
+            this.Certificationlabel.Name = "Certificationlabel";
+            this.Certificationlabel.Size = new System.Drawing.Size(108, 16);
+            this.Certificationlabel.TabIndex = 28;
+            this.Certificationlabel.Text = "CERTIFICATION";
+            // 
+            // CertificationtextBox
+            // 
+            this.CertificationtextBox.Location = new System.Drawing.Point(761, 298);
+            this.CertificationtextBox.Multiline = true;
+            this.CertificationtextBox.Name = "CertificationtextBox";
+            this.CertificationtextBox.Size = new System.Drawing.Size(218, 91);
+            this.CertificationtextBox.TabIndex = 29;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "2x2 PICTURE";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(107, 444);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 152);
+            this.pictureBox1.TabIndex = 31;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(822, 630);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(157, 35);
+            this.Savebutton.TabIndex = 32;
+            this.Savebutton.Text = "SAVE";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            // 
+            // PDFResumeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1027, 677);
+            this.Controls.Add(this.Savebutton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CertificationtextBox);
+            this.Controls.Add(this.Certificationlabel);
+            this.Controls.Add(this.AwardstextBox);
+            this.Controls.Add(this.Awardlabel);
+            this.Controls.Add(this.WorkExperiencetextBox);
+            this.Controls.Add(this.WorkExperiencelabel);
+            this.Controls.Add(this.SYCollegetextBox);
+            this.Controls.Add(this.CollegetextBox);
+            this.Controls.Add(this.Collegelabel);
+            this.Controls.Add(this.SYHighschooltextBox);
+            this.Controls.Add(this.HighSchooltextBox);
+            this.Controls.Add(this.HighSchoollabel);
+            this.Controls.Add(this.SYElementarytextBox);
+            this.Controls.Add(this.SYlabel);
+            this.Controls.Add(this.Elementarylabel);
+            this.Controls.Add(this.ElementarytextBox);
+            this.Controls.Add(this.Educationlabel);
+            this.Controls.Add(this.SkillstextBox);
+            this.Controls.Add(this.Skillslabel);
+            this.Controls.Add(this.Contactlabel);
+            this.Controls.Add(this.HometextBox);
+            this.Controls.Add(this.Homelabel);
+            this.Controls.Add(this.EmailtextBox);
+            this.Controls.Add(this.Emaillabel);
+            this.Controls.Add(this.PhonetextBox);
+            this.Controls.Add(this.Phonelabel);
+            this.Controls.Add(this.SurnametextBox);
+            this.Controls.Add(this.Surnamelabel);
+            this.Controls.Add(this.FirstNametextBox);
+            this.Controls.Add(this.FirstNamelabel);
+            this.Name = "PDFResumeForm";
+            this.Text = "PDF RESUME CREATOR";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label FirstNamelabel;
+        private System.Windows.Forms.TextBox FirstNametextBox;
+        private System.Windows.Forms.Label Surnamelabel;
+        private System.Windows.Forms.TextBox SurnametextBox;
+        private System.Windows.Forms.Label Phonelabel;
+        private System.Windows.Forms.TextBox PhonetextBox;
+        private System.Windows.Forms.Label Emaillabel;
+        private System.Windows.Forms.TextBox EmailtextBox;
+        private System.Windows.Forms.Label Homelabel;
+        private System.Windows.Forms.TextBox HometextBox;
+        private System.Windows.Forms.Label Contactlabel;
+        private System.Windows.Forms.Label Skillslabel;
+        private System.Windows.Forms.TextBox SkillstextBox;
+        private System.Windows.Forms.Label Educationlabel;
+        private System.Windows.Forms.TextBox ElementarytextBox;
+        private System.Windows.Forms.Label Elementarylabel;
+        private System.Windows.Forms.Label SYlabel;
+        private System.Windows.Forms.TextBox SYElementarytextBox;
+        private System.Windows.Forms.Label HighSchoollabel;
+        private System.Windows.Forms.TextBox HighSchooltextBox;
+        private System.Windows.Forms.TextBox SYHighschooltextBox;
+        private System.Windows.Forms.Label Collegelabel;
+        private System.Windows.Forms.TextBox CollegetextBox;
+        private System.Windows.Forms.TextBox SYCollegetextBox;
+        private System.Windows.Forms.Label WorkExperiencelabel;
+        private System.Windows.Forms.TextBox WorkExperiencetextBox;
+        private System.Windows.Forms.Label Awardlabel;
+        private System.Windows.Forms.TextBox AwardstextBox;
+        private System.Windows.Forms.Label Certificationlabel;
+        private System.Windows.Forms.TextBox CertificationtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Savebutton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace PDF_Resume_Creator
 {
-    public partial class Form1 : Form
+    public partial class PDFResumeForm : Form
     {
-        public Form1()
+        public PDFResumeForm()
         {
             InitializeComponent();
         }
+
+
     }
 }
